74066264838ac0e298368b50849b266da9bd27ea
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-07-13 02:28:24 +0530

    finished ui

---
+++ components/HomePage.js
@@ -0,0 +1,105 @@
+import styled from "styled-components";
+import Image from "next/image";
+import logo from "../public/images/mg-typing-test-logo.png";
+import bgImg from "../public/images/typing-girl.svg";
+import {
+  headingAnimationColor,
+  headingColor,
+  primaryColor,
+  secondaryColor,
+} from "../constants/color";
+import TypingCard from "./TypingCard";
+
+const HomePage = () => {
+  return (
+    <Div>
+      <MainDiv>
+        <Logo>
+          <Image src={logo} alt="Mg-Typing-Test" width="60" height="60" />
+          <h1 data-text="MG-Typing Test">MG-Typing Test</h1>
+        </Logo>
+        <MainContainer>
+          <LeftContainer>
+            <TypingCard />
+          </LeftContainer>
+          <RightContainer>
+            <Image src={bgImg} alt="typing-gril" width="" height="" />
+            <h3>
+              Developered By <br /> <span>Mistry Pavan kumar</span>
+            </h3>
+          </RightContainer>
+        </MainContainer>
+      </MainDiv>
+    </Div>
+  );
+};
+
+export default HomePage;
+
+const Div = styled.div`
+  padding: 10px;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const MainDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: ${primaryColor};
+  border-radius: 0.8rem;
+  box-shadow: 0 0 10px 3px rgba(33, 0, 87, 0.5);
+`;
+const Logo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.8rem 0;
+
+  h1 {
+    margin-left: 1rem;
+    color: ${headingColor};
+    font-size: 1.5rem;
+    position: relative;
+    display: inline-block;
+    white-space: nowrap;
+
+    ::before {
+      content: attr(data-text);
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 0;
+      border-right: 3px solid ${headingAnimationColor};
+      color: ${headingAnimationColor};
+      overflow: hidden;
+
+      animation: animate 6s linear infinite alternate;
+      --webkit-animation: animate 6s linear infinite alternate;
+
+      @keyframes animate {
+        0% {
+          width: 0;
+        }
+        50% {
+          width: 100%;
+        }
+        100% {
+          width: 0;
+        }
+      }
+    }
+
+    @media screen and (min-width: 768px) {
+      font-size: 2rem;
+    }
+  }
+`;
+
+const MainContainer = styled.div`
+  display: grid;
+  width: 1200px;
+  margin: auto;
+  grid-template-columns: repeat(2, 1fr);
+`;
+const LeftContainer = styled.div``;
+const RightContainer = styled.div``;
---
+++ components/TypingCard.js
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import styled from "styled-components";
+import Preview from "./Preview";
+import getText from "../data/getText";
+import {
+  headingColor,
+  lightSecondaryColor,
+  lightSecondaryColor1,
+  primaryColor,
+  secondaryColor,
+} from "../constants/color";
+
+const initialState = {
+  text: getText(),
+  userInput: "",
+  symbols: 0,
+  sec: 0,
+  started: false,
+  finished: false,
+};
+
+const TypingCard = () => {
+  const [state, setState] = useState(initialState);
+  const [countDown, setCountDown] = useState(60);
+
+  return (
+    <CardContainer>
+      <div className="inner">
+        <CountDown isStated={state.started} countDown={countDown}>
+          <h2>{countDown}s</h2>
+        </CountDown>
+        <Preview text={state.text} userInput={state.userInput} />
+        <TextArea />
+      </div>
+    </CardContainer>
+  );
+};
+
+export default TypingCard;
+
+const CardContainer = styled.div`
+  width: 60vw;
+  height: 100%;
+  border-radius: 1rem;
+  background-color: #fff;
+  overflow: hidden;
+
+  .inner {
+    padding: 30px;
+    position: relative;
+  }
+`;
+
+const CountDown = styled.div`
+  height: 50px;
+  width: 50px;
+  border-radius: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ isStated, countDown }) =>
+    isStated && countDown != 0 ? primaryColor : lightSecondaryColor1};
+  color: ${headingColor};
+  font-size: 0.8rem;
+  position: absolute;
+  right: 15px;
+  top: 15px;
+  transition: all 0.5s ease-in-out;
+  transform: ${({ isStated, countDown }) =>
+    isStated && countDown != 0 ? "scale(1.1)" : "scale(1)"};
+
+  h2 {
+    opacity: ${({ isStated, countDown }) =>
+      isStated && countDown != 0 ? 1 : 0.5};
+    z-index: 10;
+  }
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 10vh;
+  border-radius: 15px;
+  margin-top: 2rem;
+  border: none;
+  outline: none;
+  box-shadow: 0 0 10px 2px rgba(0, 0, 0, 0.2);
+  opacity: 0.7;
+  font-size: 1rem;
+  padding: 10px 1rem;
+  resize: none;
+  line-height: 1.5rem;
+
+  scrollbar-face-color: #ff8c00;
+
+  :focus {
+    background-color: ${lightSecondaryColor1};
+    box-shadow: 0 0 10px 1px rgba(154, 129, 225, 0.8);
+  }
+`;
