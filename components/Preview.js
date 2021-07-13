bc51996e4c1ab1ef2555368d7c7f3b52219544bd
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-07-13 12:14:03 +0530

    Made it responsive

--- components/Preview.js
+++ components/Preview.js
@@ -34,11 +34,16 @@ export default Preview;
 
 const MainDiv = styled.div`
   padding: 1rem;
-  height: 40vh;
+  height: auto;
+  width: 100%;
   background-color: ${lightSecondaryColor};
   border-radius: 15px;
   box-shadow: 0 0 10px 1px rgba(154, 129, 225, 0.8);
   color: #ffff;
+
+  @media (min-width: 768px) {
+    height: 40vh;
+  }
 
   .textSize {
     font-size: 1.2rem;
