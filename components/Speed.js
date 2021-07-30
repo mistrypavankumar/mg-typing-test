c82535974b7d6e3939f7106e559e35c2bff31eb0
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-07-30 13:32:21 +0530

    fixed error

--- components/Speed.js
+++ components/Speed.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef,useEffect } from "react";
 import styled from "styled-components";
 import { headingAnimationColor, primaryColor } from "../constants/color";
 
